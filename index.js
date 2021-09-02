0cc2682338435f6e25036cb5a218e4503120a37c
Abhishek <eduandabhi@gmail.com>
2021-09-02 18:09:25 +0530

    Login Implemented

--- index.js
+++ index.js
@@ -5,5 +5,8 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App.js';
 import {name as appName} from './app.json';
+import Amplify, {Auth} from 'aws-amplify';
+
+// >>New - Configuring Auth Module
 
 AppRegistry.registerComponent(appName, () => App);
--- src/App.js
+++ src/App.js
@@ -13,7 +13,12 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import {NavigationContainer} from '@react-navigation/native';
 import 'react-native-gesture-handler';
+import {withAuthenticator} from 'aws-amplify-react-native';
+import Amplify from 'aws-amplify';
+// Get the aws resources configuration parameters
+import awsconfig from './aws-exports'; // if you are using Amplify CLI
 
+Amplify.configure(awsconfig);
 import MyTabs from './components/MyTabs';
 
 const App: () => Node = () => {
@@ -27,4 +32,4 @@ const App: () => Node = () => {
   );
 };
 
-export default App;
+export default withAuthenticator(App);
